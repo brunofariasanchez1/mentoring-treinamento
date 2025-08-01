16274c4012a19770d590eadfdc1791ff33ad08f6
Bruno Faria Sanchez <804620784@OSPMP.CORP>
2025-08-01 18:39:43 -0300

    Criação do CRUD, usando o verbo RETRIEVE.

--- mentoring/src/main/java/br/com/orbitall/mentoring/services/CardService.java
+++ mentoring/src/main/java/br/com/orbitall/mentoring/services/CardService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 @Service
 @Slf4j
@@ -27,6 +28,16 @@ public class CardService {
 
         return this.repository.save(card);
     }
+
+    public Iterable<Card> list(){
+        return this.repository.findAll();
+    }
+
+    public Card retrieve(UUID id){
+      return  this.repository.findById(id).get();
+    }
+
+
 
 
 }
